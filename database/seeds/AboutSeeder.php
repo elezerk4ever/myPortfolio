5bac261406da5f08697322ff0cf95ce24b1f85bf
elezerk4ever <williamgalas03@gmail.com>
2020-06-08 22:53:39 +0800

    finishing the aboutseeder

--- database/seeds/AboutSeeder.php
+++ database/seeds/AboutSeeder.php
@@ -12,7 +12,16 @@ class AboutSeeder extends Seeder
     public function run()
     {
         About::create([
-            
-        ])
+            'user_id'=>1,
+            'intro'=>"Please edit me!",
+            "bdate"=>"04/03/2000",
+            "img"=>"https://shorturl.at/mqHNW",
+            "website"=>"place your website link here",
+            "phone"=>"09096524461",
+            "city"=>"tarlac",
+            "age"=>"20",
+            "isFreelance"=>false,
+            "objectives"=>"write you objectives here"
+        ]);
     }
 }
